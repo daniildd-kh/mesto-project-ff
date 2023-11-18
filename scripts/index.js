7de810def2816e7400e43b06f5cba989273e0b47
Khromov Daniil <khromov865@mail.ru>
2023-11-18 20:35:41 +0300

    added delete and make card features

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-// @todo: Темплейт карточки
+const cardList = document.querySelector(".places__list");
+
+function makeCard(cardTitle, cardImg) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardDelete = cardElement.querySelector(".card__delete-button");
+
+  function deleteCard() {
+    const cardItem = cardDelete.closest(".card");
+    cardItem.remove();
+  }
+
+  cardDelete.addEventListener("click", deleteCard);
+
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardImgElement = cardElement.querySelector(".card__image");
+
+  cardTitleElement.textContent = cardTitle;
+  cardImgElement.src = cardImg;
+  cardImgElement.alt = cardTitle;
+
+  cardList.append(cardElement);
+}
+for (const initialCard of initialCards) {
+  makeCard(initialCard.name, initialCard.link);
+}
 
 // @todo: DOM узлы
 
